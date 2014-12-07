0d5763ad6fa5505e53aef71a3046d9561a28d637
rahulpalamuttam <rahulpalamut@gmail.com>
2014-12-06 18:38:39 -0800

    1)testContains checks for both valid and invalid ids 2)Updated JavaDocs 3)Reformatted Code

--- src/test/java/org/sdsc/pdbproject/UnrelIDHashTest.java
+++ src/test/java/org/sdsc/pdbproject/UnrelIDHashTest.java
@@ -1,53 +1,57 @@
-//Written by Rahul Palamuttam
 package org.sdsc.pdbproject;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 /**
- * This class is used to test the UnrelIDHash table functionality
+ * Test class for UnrelIDHash table functionality
+ *
+ * @author Rahul Palamuttam
  */
 public class UnrelIDHashTest {
-    private static String TESTER="UnrelIDHashTest: ";
     private static String filename = "unreleasedPDBid.csv";
     private static UnrelIDHash TestHash;
-    
-    // Check if appropriate files are available
+
+    /**
+     * Instantiates a new Unrel iD hash test.
+     */
     public UnrelIDHashTest() {
-	TestHash = new UnrelIDHash(filename);
+        TestHash = new UnrelIDHash(filename);
     }
 
-    //Check if constructor worked.
+    /**
+     * Tests if the Hash table was created.
+     */
     @Test
-    public void testcreateUnreleasedHash(){
-	String assertString = "UnrelIDHash was not called: ";
-	assertNotNull(assertString, TestHash);
+    public void testcreateUnreleasedHash() {
+        String assertString = "UnrelIDHash was not called: ";
+        assertNotNull(assertString, TestHash);
     }
 
-    //Check if toString works
+    /**
+     * Check if toString works.
+     */
     @Test
-    public void testtoString(){
-	String assertString = "UnredIDHash.toString returned null: ";
-	String testString = null;
-	testString = TestHash.toString();
-	System.out.println(testString);
-	assertNotNull(assertString, testString);
+    public void testtoString() {
+        String assertString = "UnredIDHash.toString returned null: ";
+        String testString = TestHash.toString();
+        System.out.println(testString);
+        assertNotNull(assertString, testString);
     }
-    
-    //Check if contains works
+
+
+    /**
+     * Check if contains works.
+     */
     @Test
-    public void testcontains(){
-	String assertString = "UnrelIDHash.testcontains() returned true";
-	String random = "sdfs";
-	Boolean notTrue = TestHash.contains(random);
-	assertFalse(assertString, notTrue);
-	
+    public void testcontains() {
+        String assertString = "UnrelIDHash.testcontains() said: ";
+        String random = "sdfs";
+        String existing = "1ujh";
+        Boolean notTrue = TestHash.contains(random);
+        Boolean isTrue = TestHash.contains(existing);
+        assertFalse(assertString + random + " exists", notTrue);
+        assertTrue(assertString + random + "does not exist", isTrue);
+
     }
 }
