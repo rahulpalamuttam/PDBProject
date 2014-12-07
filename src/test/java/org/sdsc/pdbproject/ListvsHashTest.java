9650ddc00947dea073be7646982238a5e35ae1fe
rahulpalamuttam <rahulpalamut@gmail.com>
2014-12-06 19:02:40 -0800

    Updated JavaDocs

--- src/test/java/org/sdsc/pdbproject/ListvsHashTest.java
+++ src/test/java/org/sdsc/pdbproject/ListvsHashTest.java
@@ -1,106 +1,118 @@
-//Written by Rahul Palamuttam
 package org.sdsc.pdbproject;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 /**
- * This class is used to test the performance times
+ * Runs performance tests for comparing
+ * a custom hashtable to a list.
  * of the UnrelIDHash vs the UnrelIDList
+ *
+ * @author Rahul Palamuttam
  */
 
 public class ListvsHashTest {
-    public String TESTER="UnrelIDHashTest: ";
-    public String filename = "unreleasedPDBid.csv";
     private static UnrelIDHash TestHash;
     private static UnrelID TestList;
-    
-    // load the two datastructures
-    public ListvsHashTest(){
-	long beforeHash = System.nanoTime();
-	TestHash = new UnrelIDHash(filename);
-	long afterHash = System.nanoTime();
-	TestList = new UnrelID(filename);
-	long afterList = System.nanoTime();
-	// System.out.println("Nanoseconds taken to create HashObject: " + 
-	// 		   (afterHash - beforeHash));
-	// System.out.println("Nanoseconds taken to create ListObject: " +
-	// 		   (afterList - afterHash));
+    public String filename = "unreleasedPDBid.csv";
+    public long HashTableCreateTime;
+    public long ListCreateTime;
+
+    /**
+     * Creates a TestList and TestHashTable.
+     * Also loads run times for creating the structures.
+     */
+    public ListvsHashTest() {
+        long beforeHash = System.nanoTime();
+        TestHash = new UnrelIDHash(filename);
+        long afterHash = System.nanoTime();
+        TestList = new UnrelID(filename);
+        long afterList = System.nanoTime();
+        HashTableCreateTime = afterHash - beforeHash;
+        ListCreateTime = afterList - beforeHash;
     }
 
-    // Compares the run time of finding an invalid object
+    /**
+     * Compares the run time of finding invalid ids.
+     * The list structure must iterate through all its entries.
+     * The hash table doesn't.
+     * Hence the hasTime < listTime.
+     */
     @Test
-    public void FindUnavailable(){
-	String random = "0j.;";
-	long beforeHash = System.nanoTime();
-	TestHash.contains(random);
-	long afterHash = System.nanoTime();
-	TestList.contains(random);
-	long afterList = System.nanoTime();
-	long hashTime = afterHash - beforeHash;
-	long listTime = afterList - afterHash;
-	String assertString = "Nanoseconds to find list[NaN] in Hash: " + hashTime + "\n";
-	assertString += "Nanoseconds to find list[NaN] in List: " + listTime + "\n";
-	System.out.println(assertString);
-	assertTrue(assertString, hashTime < listTime);
+    public void FindUnavailableTest() {
+        String random = "0j.;";
+        long beforeHash = System.nanoTime();
+        TestHash.contains(random);
+        long afterHash = System.nanoTime();
+        TestList.contains(random);
+        long afterList = System.nanoTime();
+        long hashTime = afterHash - beforeHash;
+        long listTime = afterList - afterHash;
+        String assertString = "Nanoseconds to find list[NaN] in Hash: " + hashTime + "\n";
+        assertString += "Nanoseconds to find list[NaN] in List: " + listTime + "\n";
+        System.out.println(assertString);
+        assertTrue(assertString, hashTime < listTime);
     }
 
-    // Compares the run time of finding the first object in the List
+    /**
+     * Compares the run time of finding the first object in the List.
+     * Since it is the first object hashTime should be greater than listTime.
+     */
     @Test
-    public void FindEarlyAvailable(){
-	String random = "1uJh";
-	long beforeHash = System.nanoTime();
-	TestHash.contains(random);
-	long afterHash = System.nanoTime();
-	TestList.contains(random);
-	long afterList = System.nanoTime();
-	long hashTime = afterHash - beforeHash;
-	long listTime = afterList - afterHash;
-	String assertString = "Nanoseconds to find list[first] in Hash: " + hashTime + "\n";
-	assertString += "Nanoseconds to find list[first] in List: " + listTime + "\n";
-	System.out.println(assertString);
-	assertTrue(assertString, hashTime > listTime);
+    public void FindFirst() {
+        String random = "1uJh";
+        long beforeHash = System.nanoTime();
+        TestHash.contains(random);
+        long afterHash = System.nanoTime();
+        TestList.contains(random);
+        long afterList = System.nanoTime();
+        long hashTime = afterHash - beforeHash;
+        long listTime = afterList - afterHash;
+        String assertString = "Nanoseconds to find list[first] in Hash: " + hashTime + "\n";
+        assertString += "Nanoseconds to find list[first] in List: " + listTime + "\n";
+        System.out.println(assertString);
+        assertTrue(assertString, hashTime > listTime);
 
     }
 
-    // Compares the run time of finding the Last object in the List
+    /**
+     * Find last object in the list.
+     * Since it is the last object in the list,
+     * hashTime should be less than the listTime.
+     */
     @Test
-    public void FindLast(){
-	String random = "4WM8";
-	long beforeHash = System.nanoTime();
-	TestHash.contains(random);
-	long afterHash = System.nanoTime();
-	TestList.contains(random);
-	long afterList = System.nanoTime();
-	long hashTime = afterHash - beforeHash;
-	long listTime = afterList - afterHash;
-	String assertString = "Nanoseconds to find list[last] in Hash: " + hashTime + "\n";
-	assertString += "Nanoseconds to find early list[last] in List: " + listTime + "\n";
-	System.out.println(assertString);
-	assertTrue(assertString, hashTime < listTime);
+    public void FindLast() {
+        String random = "4WM8";
+        long beforeHash = System.nanoTime();
+        TestHash.contains(random);
+        long afterHash = System.nanoTime();
+        TestList.contains(random);
+        long afterList = System.nanoTime();
+        long hashTime = afterHash - beforeHash;
+        long listTime = afterList - afterHash;
+        String assertString = "Nanoseconds to find list[last] in Hash: " + hashTime + "\n";
+        assertString += "Nanoseconds to find early list[last] in List: " + listTime + "\n";
+        System.out.println(assertString);
+        assertTrue(assertString, hashTime < listTime);
     }
 
-    // Compares the run time of finding the 5th object in the List
+    /**
+     * Compare search times for finding two-hundred-ninetieth list element.
+     */
     @Test
-    public void FindTwoNinety(){
-	String random = "4D1R";
-	long beforeHash = System.nanoTime();
-	TestHash.contains(random);
-	long afterHash = System.nanoTime();
-	TestList.contains(random);
-	long afterList = System.nanoTime();
-	long hashTime = afterHash - beforeHash;
-	long listTime = afterList - afterHash;
-	String assertString = "Nanoseconds to find list[290] in Hash: " + hashTime + "\n";
-	assertString += "Nanoseconds to find early list[290] in List: " + listTime + "\n";
-	System.out.println(assertString);
-	assertTrue(assertString, hashTime < listTime);
+    public void FindTwoNinety() {
+        String random = "4D1R";
+        long beforeHash = System.nanoTime();
+        TestHash.contains(random);
+        long afterHash = System.nanoTime();
+        TestList.contains(random);
+        long afterList = System.nanoTime();
+        long hashTime = afterHash - beforeHash;
+        long listTime = afterList - afterHash;
+        String assertString = "Nanoseconds to find list[290] in Hash: " + hashTime + "\n";
+        assertString += "Nanoseconds to find early list[290] in List: " + listTime + "\n";
+        System.out.println(assertString);
+        assertTrue(assertString, hashTime < listTime);
     }
 }
